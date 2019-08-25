f76e787217edce0a061c2d5a00c9c298e94a12b5
Daniel Silverio <daniel.silverio@mariachi.io>
2019-08-25 02:13:17 -0500

    Se agrego el endpoin de registro

--- entities/Users.js
+++ entities/Users.js
@@ -1,7 +1,12 @@
+"use strict";
+require("dotenv").config();
 let co = require("co");
 let DBEntity = require("./DB");
+let jwt = require("jsonwebtoken");
 let DB = new DBEntity();
-
+let { tap, tapReject } = require("../utils/promiseUtils");
+let { getSum } = require("../utils/shasum");
+let muebles = require("../stock/muebles");
 class User {
   constructor(id) {
     if (!id) {
@@ -10,14 +15,78 @@ class User {
     this.id = String(id);
   }
 
-  static getData() {
+  getToken() {
+    let self = this.id;
     return co(function*() {
-      let collection = yield DB.getCollection("userhack");
-      if (collection) {
-        yield collection.insertOne({user: "12345678"})
-      } else {
-        console.log("NO");
-      }
+      return jwt.sign({ id: self.id }, process.env.SECRET_KEY, {});
+    })
+      .then(tap.bind(console.log(null, "get Token ok")))
+      .catch(tapReject.bind(console.log(null, "get Token error")));
+  }
+
+  static getUserByToken(token) {
+    return co(function*() {
+      let decoded = jwt.verify(token, process.env.SECRET_KEY);
+      return new User(decoded.id);
+    })
+      .then(tap.bind(null, console.log.bind(null, "getUserByToken ok")))
+      .catch(
+        tapReject.bind(null, console.error.bind(null, "getUserByToken error"))
+      );
+  }
+
+  static authenticationMiddleware(req, res, next) {
+    let token = req.header("Authorization");
+    let error = new Error();
+    error.directions = "La sesión no es válida, o no existe.";
+    if (!token) {
+      next(error);
+      return;
+    }
+    User.getUserByToken(token)
+      .then(user => {
+        req.user = user;
+        next();
+      })
+      .catch(err => {
+        err.directions = error.directions;
+        next(err);
+      });
+  }
+
+  static register(email, password) {
+    return co(function*() {
+      let collection = yield DB.getCollection("user");
+      let userInfo = yield DB.getCollection("userInfo");
+      let user = yield collection.findOne({ email: email });
+      let pass = getSum(password);
+      let result = yield collection.insertOne({
+        email: email,
+        password: pass
+      });
+      user = new User(result.insertedId);
+      yield userInfo.insertOne({ user: user.id });
+      return;
+    });
+  }
+
+  static saveInfo(params) {
+    return co(function*() {
+      let collection = yield DB.getCollection("userInfo");
+      
+      yield collection.updateOne(
+        { user: "5d621e56fc2cab1784dd43ac" },
+        {
+          $set: {
+            nombre: params.nombre,
+            apellidoPaterno: params.apellidoPaterno,
+            apellidoMaterno: params.apellidoMaterno,
+            direccion: params.direccion,
+            ciudad: params.ciudad,
+            producto: params.producto
+          }
+        }
+      );
       return "JAJA";
     });
   }
